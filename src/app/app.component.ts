7244f7352bb2d5319d1de157b9c7b74858768686
Четайкин Влад <dsa.vlad@list.ru>
2026-06-21 17:24:19 +0300

    Add counter component with standalone architecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter.component'; 
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone: true,  
+  imports: [CounterComponent],  
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FirstEvoApp';
+  title: string = 'Hello';
 }
